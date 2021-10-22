23e58f79965a65c70d4e31ed7950e4b86274e003
Winzacar <moralesriky97@gmail.com>
2021-10-22 14:53:22 -0400

    security update

--- js/firebase_creds.js
+++ js/firebase_creds.js
@@ -1,11 +1,5 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyA5x7CVw_uJ71yDkdw1pqkITpP90jHOO6w",
-    authDomain: "multi-page-59901.firebaseapp.com",
-    projectId: "multi-page-59901",
-    storageBucket: "multi-page-59901.appspot.com",
-    messagingSenderId: "14701181724",
-    appId: "1:14701181724:web:59be7fe6887f7b833ed8d9",
-    measurementId: "G-CRBNTV4D5M"
+    /*your credentials of firebase*/
   };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
